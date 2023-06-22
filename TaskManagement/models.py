d08c53b62e6aaa6e51213ca6acb705c1878c16d6
a.lazev@transdata.no <a.lazev@transdata.no>
2023-06-22 19:19:34 +0300

    Add choices to model

--- TaskManagement/models.py
+++ TaskManagement/models.py
@@ -7,10 +7,14 @@ UserModel = get_user_model()
 
 
 class TasksModel(models.Model):
+    STATUS_CHOICES = [
+        (False, 'Pending'),
+        (True, 'Completed'),
+    ]
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=64)
     description = models.TextField(blank=True)
-    execution_status = models.BooleanField(default=False)
+    execution_status = models.BooleanField(default=False, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     reporter = models.ForeignKey(UserModel, on_delete=models.PROTECT, related_name='reporter_user')
