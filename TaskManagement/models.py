b4f22588c8dcf156399d0b018d7a4e820128dd06
a.lazev@transdata.no <a.lazev@transdata.no>
2023-06-22 17:40:59 +0300

    TaskModel

--- TaskManagement/models.py
+++ TaskManagement/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class TasksModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    execution_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reporter = models.ForeignKey(UserModel, on_delete=models.PROTECT, related_name='reporter_user')
+    assignee = models.ForeignKey(UserModel, on_delete=models.PROTECT, related_name='assignee_user')
+
+    def __str__(self):
+        return f'{self.reporter} - {self.title}'
