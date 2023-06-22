0e0a2d74739967c259cbafd2577f392d1b5dbc0e
a.lazev@transdata.no <a.lazev@transdata.no>
2023-06-22 17:40:48 +0300

    Create task form

---
+++ TaskManagement/forms.py
@@ -0,0 +1,25 @@
+from django.forms import TextInput, Textarea, Select
+from django import forms
+
+from TaskManagement.models import TasksModel, UserModel
+
+
+class CreateTaskForm(forms.ModelForm):
+    reporter = forms.ModelChoiceField(queryset=UserModel.objects.all(), widget=Select(attrs={'class': 'form-control',
+                                                                                             'placeholder': 'Select reporter'}))
+    assignee = forms.ModelChoiceField(queryset=UserModel.objects.all(), widget=Select(attrs={'class': 'form-control',
+                                                                                             'placeholder': 'Select assignee'}))
+
+    class Meta:
+        model = TasksModel
+        fields = ['title', 'description', 'reporter', 'assignee']
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Type title'
+            }),
+            'description': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Type description'
+            })
+        }
