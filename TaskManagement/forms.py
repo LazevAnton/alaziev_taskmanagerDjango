65e6fce0cac7d8fd4f0fdee3d122819bcf502423
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-03 18:36:29 +0300

    удалил формы регистрации и логина

--- TaskManagement/forms.py
+++ TaskManagement/forms.py
@@ -29,29 +29,3 @@ class CreateTaskForm(forms.ModelForm):
                 'placeholder': 'Type description'
             })
         }
-
-
-class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    password1 = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    password2 = forms.CharField(
-        label='Repeat your password',
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
-
-
-class LoginUserForm(forms.Form):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
