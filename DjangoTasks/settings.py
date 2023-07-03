b16c119db719079525a3a7cc5a8a5285eb56a7f0
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-03 18:38:39 +0300

    AUTH_USER_MODEL = 'TaskManagement.UserModel'

--- DjangoTasks/settings.py
+++ DjangoTasks/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'TaskManagement.CustomUserModel'
+AUTH_USER_MODEL = 'TaskManagement.UserModel'
 
 
 AUTH_PASSWORD_VALIDATORS = [
