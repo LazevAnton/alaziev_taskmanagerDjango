e9b7cd7da39d2068af2201238e0eab3e8d7bb463
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-10 22:48:20 +0300

    Edit some forms

--- TaskManagement/forms.py
+++ TaskManagement/forms.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.forms import TextInput, Textarea, Select, CheckboxInput
 from django import forms
 from TaskManagement.models import TasksModel, UserModel
@@ -7,11 +6,11 @@ from TaskManagement.models import TasksModel, UserModel
 class CreateTaskForm(forms.ModelForm):
     reporter = forms.ModelChoiceField(
         queryset=UserModel.objects.all(),
-        widget=Select(attrs={'class': 'form-control', 'placeholder': 'Select reporter'})
+        widget=Select(attrs={'class': 'form-control'})
     )
     assignee = forms.ModelChoiceField(
         queryset=UserModel.objects.all(),
-        widget=Select(attrs={'class': 'form-control', 'placeholder': 'Select assignee'})
+        widget=Select(attrs={'class': 'form-control'})
     )
     execution_status = forms.ChoiceField(
         choices=TasksModel.STATUS_CHOICES,
@@ -35,47 +34,42 @@ class CreateTaskForm(forms.ModelForm):
         }
 
 
-class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=32)
-    first_name = forms.CharField(max_length=32, required=False)
-    last_name = forms.CharField(max_length=32, required=False)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+class UserRegisterForm(forms.ModelForm):
+    confirm_password = forms.CharField()
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        try:
-            UserModel.objects.get(username=username)
-            raise ValidationError(f'{username}, already registered')
-        except UserModel.DoesNotExist:
-            return username
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        try:
-            UserModel.objects.get(email=email)
-            raise ValidationError(f'{email}, already registered')
-        except UserModel.DoesNotExist:
-            return email
+    class Meta:
+        model = UserModel
+        fields = [
+            'username', 'first_name',
+            'last_name', 'email',
+            'password'
+        ]
+        widgets = {
+            'password': forms.PasswordInput,
+            'confirm_password': forms.PasswordInput
+        }
+    #
+    # def clean_password(self):
+    #     password = self.cleaned_data['password']
+    #     confirm_password = self.cleaned_data.get('confirm_password')
+    #     if password != confirm_password:
+    #         self.add_error('confirm_password', 'Passwords do not match')
+    #     return confirm_password
 
     def clean(self):
         password = self.cleaned_data.get('password')
         confirm_password = self.cleaned_data.get('confirm_password')
-        if password and confirm_password and password != confirm_password:
-            self.add_error('confirm_password', 'Passwords dont match')
-
-    def save(self):
-        username = self.cleaned_data['username']
-        password = self.cleaned_data['password']
-        email = self.cleaned_data['email']
-        first_name = self.cleaned_data.get('first_name')
-        last_name = self.cleaned_data.get('last_name')
-        user = UserModel.objects.create_user(username=username, password=password, email=email,
-                                             first_name=first_name, last_name=last_name)
-        return user
+        if password != confirm_password:
+            self.add_error('confirm_password', 'Passwords do not match')
 
 
-class LoginUserForm(forms.Form):
-    username = forms.CharField(max_length=32)
-    password = forms.CharField(widget=forms.PasswordInput)
+class LoginUserForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = [
+            'username',
+            'password'
+        ]
+        widgets = {
+            'password': forms.PasswordInput
+        }
