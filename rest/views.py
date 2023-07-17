71b8845425b97e1c36ae11407137ebffcb9fd7c9
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-17 17:10:32 +0300

    add class CustomObtainPairVIew

--- rest/views.py
+++ rest/views.py
@@ -1,17 +1,28 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
 from TaskManagement.models import TasksModel, UserModel
-from rest.serializers import TaskModelSerializer, UserModelSerializer
+from rest.serializers import TaskModelSerializer, UserModelSerializer, CustomTokenObtainPairSerializer
+
+
+class CustomObtainPairVIew(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 class TaskListCreateAPIView(generics.ListCreateAPIView):
     queryset = TasksModel.objects.all()
     serializer_class = TaskModelSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TasksModel.objects.all()
     serializer_class = TaskModelSerializer
     lookup_field = 'uuid'
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class UserListCreateAPIView(generics.ListCreateAPIView):
@@ -23,3 +34,5 @@ class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserModelSerializer
     lookup_field = 'id'
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
