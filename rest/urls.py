2441a8b908639abcee7645006793f0ccf3080de8
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-17 17:10:10 +0300

    urls for token and refresh token

--- rest/urls.py
+++ rest/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from rest.views import TaskListCreateAPIView, TaskRetrieveUpdateDestroyAPIView, UserListCreateAPIView, \
-    UserRetrieveUpdateDestroyAPIView
+    UserRetrieveUpdateDestroyAPIView, CustomObtainPairVIew
 
 urlpatterns = [
+    path('token/', CustomObtainPairVIew.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('tasks/', TaskListCreateAPIView.as_view(), name='TasksList_CreateTask'),
     path('tasks/<uuid:uuid>/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='TaskUpdateDestroy'),
     path('users/', UserListCreateAPIView.as_view(), name='CreateUser'),
