180c276a32cbb0473936a6942aa49566137e3a50
a.lazev@transdata.no <a.lazev@transdata.no>
2023-07-17 17:11:00 +0300

    CustomTokenObtainPairSerializer for token create

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from TaskManagement.models import TasksModel, UserModel
 
 
@@ -13,3 +15,11 @@ class TaskModelSerializer(serializers.ModelSerializer):
         model = TasksModel
         fields = ['uuid', 'title', 'description', 'execution_status',
                   'created_at', 'updated_at', 'reporter', 'assignee']
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
